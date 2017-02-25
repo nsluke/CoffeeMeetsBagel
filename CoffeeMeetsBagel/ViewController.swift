0ae56ef5ac294d70dc86d459ed739fd0363068e3
Luke Solomon <luke@yidio.com>
2017-02-24 21:46:30 -0800

    master  initial commit. Getting everything set up.

---
+++ CoffeeMeetsBagel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoffeeMeetsBagel
+//
+//  Created by Luke Solomon on 2/24/17.
+//  Copyright © 2017 Luke Solomon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-CoffeeMeetsBagel/Pods-CoffeeMeetsBagel-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CoffeeMeetsBagel : NSObject
+@end
+@implementation PodsDummy_Pods_CoffeeMeetsBagel
+@end
