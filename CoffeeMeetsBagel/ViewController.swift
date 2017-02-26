87857689d872b36d4228409b7a1ccc23eba79f0e
Luke Solomon <luke@yidio.com>
2017-02-26 11:02:51 -0800

    Trying to get Koloda to work.

--- CoffeeMeetsBagel/ViewController.swift
+++ CoffeeMeetsBagel/ViewController.swift
@@ -7,19 +7,51 @@
 //
 
 import UIKit
+import Koloda
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var kolodaView: KolodaView!
+
+    var images:[UIImage]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        kolodaView.dataSource = self
+        kolodaView.delegate = self
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
+extension ViewController: KolodaViewDelegate {
+    func kolodaDidRunOutOfCards(koloda: KolodaView) {
+//        dataSource.reset()
+    }
+    
+    func koloda(koloda: KolodaView, didSelectCardAtIndex index: UInt) {
+        UIApplication.shared.openURL(NSURL(string: "http://yalantis.com/")! as URL)
+    }
+}
+
+extension ViewController: KolodaViewDataSource {
+    
+    func kolodaNumberOfCards(_ koloda: KolodaView) -> Int {
+        //        return images.count
+        return 1
+    }
+
+    func koloda(_ koloda: KolodaView, viewForCardAtIndex index: UInt) -> UIView {
+        return UIImageView(image: images[Int(index)])
+    }
+    
+    func koloda(_ koloda: KolodaView, viewForCardOverlayAtIndex index: UInt) -> OverlayView? {
+        return Bundle.main.loadNibNamed("OverlayView", owner: self, options: nil)?[0] as? OverlayView
+    }
+}
--- Podfile
+++ Podfile
@@ -3,6 +3,10 @@
 
 target 'CoffeeMeetsBagel' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+
+  pod 'Koloda'
+
+
   use_frameworks!
 
   # Pods for CoffeeMeetsBagel
